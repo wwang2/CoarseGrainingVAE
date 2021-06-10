e7b12fbc394b15d9bafe6127b0061e279651ac34
wwang2 <wwj@mit.edu>
2021-06-10 14:38:13 -0400

    compute bonds

--- src/plots.py
+++ src/plots.py
@@ -6,6 +6,7 @@ import torch
 
 import pyemma
 import mdshare
+import mdtraj as md
 
 import matplotlib.pyplot as plt 
 import matplotlib
@@ -46,3 +47,38 @@ def ramachandran_plot(xyzs, atomic_nums):
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=20)
     cbar.set_label(label='kT', size=20)
+
+def get_bond_name(bond):
+    atom1_res = bond.atom1.residue.name
+    atom2_res = bond.atom2.residue.name
+
+    atom1_el = bond.atom1.name
+    atom2_el = bond.atom2.name
+
+    bond_name = '{}-{} - {}-{}'.format(atom1_res, atom1_el, atom2_res, atom2_el)
+    
+    return bond_name
+
+def is_backbone_bond(bond):
+    
+    return bond.atom1.is_backbone and bond.atom2.is_backbone
+
+
+
+def get_bonds(traj, pdb, backbone = True):
+
+    bond_indices = []
+    bond_names = []
+
+    # get backbond distances 
+    for bond in peptide.top.bonds:
+
+        if is_backbone_bond(bond):
+            bond_indices.append([bond.atom1.index ,bond.atom2.index])
+            bond_names.append(get_bond_name(bond))
+
+    bond_indices = np.array(bond_indices)
+    bonds = md.compute_distances(traj, bond_indices)
+    
+    bonds = bonds * 10.0 # to Angstroms 
+    return bonds, bond_names
