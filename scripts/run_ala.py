74ce210931472cbc927417eb4c820536bd0eb312
wwang2 <wwj@mit.edu>
2021-06-11 12:43:17 -0400

    record cv score

--- scripts/run_ala.py
+++ scripts/run_ala.py
@@ -5,7 +5,6 @@ sys.path.append("/home/wwj/Repo/packages/NeuralForceField/")
 
 import os 
 import argparse 
-
 from data import *
 from model import * 
 from conv import * 
@@ -13,20 +12,16 @@ from datasets import *
 from utils import * 
 from plots import *
 from sampling import * 
-
 import torch
 from torch import nn
 from torch.nn import Sequential 
 import numpy as np
 import copy
-
 from nff.utils.scatter import scatter_add
 from torch_scatter import scatter_mean
-
 from nff.train import batch_to
 from tqdm import tqdm 
 from torch.utils.data import DataLoader
-
 from sklearn.model_selection import KFold
 
 parser = argparse.ArgumentParser()
@@ -39,11 +34,13 @@ parser.add_argument("-n_rbf", type=int, default=10)
 parser.add_argument("-cutoff", type=float, default=4.0)
 parser.add_argument("-enc_nconv", type=int, default=4)
 parser.add_argument("-dec_nconv", type=int, default=4 )
-parser.add_argument("-batch_size", type=int, default=64 )
+parser.add_argument("-batch_size", type=int, default=64)
+parser.add_argument("-nepochs", type=int, default=2)
+parser.add_argument("-ndata", type=int, default=200)
 parser.add_argument("-beta", type=float, default=0.001)
+parser.add_argument("-nsplits", type=int, default=5)
 parser.add_argument("--randommap", action='store_true', default=False)
 params = vars(parser.parse_args())
-
 
 working_dir = params['logdir']
 device  = params['device']
@@ -56,10 +53,12 @@ enc_nconv  = params['enc_nconv']
 dec_nconv  = params['dec_nconv']
 batch_size  = params['batch_size']
 beta  = params['beta']
+nsplits = params['nsplits']
+ndata = params['ndata']
 
 # generate mapping 
 if params['randommap']:
-    mapping = get_random_mapping()
+    mapping = get_random_mapping(n_cgs, n_atoms)
 else:
     mapping = get_mapping('dipeptide', 2.0, n_atoms, n_cgs)
 
@@ -67,14 +66,15 @@ else:
 atomic_nums, dataset = get_alanine_dipeptide_dataset(cutoff,
                                                      label='dipeptide',
                                                      mapping=mapping,
-                                                     n_frames=50, 
+                                                     n_frames=ndata, 
                                                      n_cg=n_cgs)
 
 # create subdirectory 
 if not os.path.isdir(working_dir):
     os.mkdir(working_dir)
     
-kf = KFold(n_splits=5)
+kf = KFold(n_splits=nsplits)
+cv_rmsd = []
 
 for i, (train_index, test_index) in enumerate(kf.split(list(range(len(dataset))))):
 
@@ -107,15 +107,14 @@ for i, (train_index, test_index) in enumerate(kf.split(list(range(len(dataset)))
     
     model.train()
 
-    for epoch in range(1):
+    for epoch in range(nsplits):
         # train
         mean_kl, mean_recon, xyz_train, xyz_train_recon = loop(trainloader, optimizer, device,
                                                    model, beta, epoch, train=True)
         scheduler.step(mean_recon)
         
-        
+
     # save sampled geometries 
-    
     trainloader = DataLoader(trainset, batch_size=128, collate_fn=CG_collate, shuffle=True)
     train_true_xyzs, train_recon_xyzs, mu, sigma = get_all_true_reconstructed_structures(trainloader, 
                                                                                          device,
@@ -144,3 +143,15 @@ for i, (train_index, test_index) in enumerate(kf.split(list(range(len(dataset)))
     dump_numpy2xyz(test_samples, atomic_nums, os.path.join(split_dir, 'test_samples.xyz'))
     dump_numpy2xyz(test_true_xyzs, atomic_nums, os.path.join(split_dir, 'test_original.xyz'))
     dump_numpy2xyz(test_recon_xyzs, atomic_nums, os.path.join(split_dir, 'test_recon.xyz'))
+
+
+    # compute loss and metrics 
+    test_dxyz = (test_recon_xyzs - test_true_xyzs).reshape(-1)
+    unaligned_test_rmsd = np.sqrt(np.power(test_recon_xyzs, 2)).mean()
+
+    # reconsturction loss 
+    cv_rmsd.append(unaligned_test_rmsd)
+
+# save CV score 
+np.savetxt(os.path.join(working_dir, 'cv_rmsd.txt'), np.array(cv_rmsd))
+
