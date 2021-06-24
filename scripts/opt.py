e418fea09e25157b7a08cc049d6fb4bbe29bed82
wwang2 <wwj@mit.edu>
2021-06-24 13:46:22 -0400

    param range/options update

--- scripts/opt.py
+++ scripts/opt.py
@@ -16,10 +16,12 @@ if params['dry_run']:
     token = 'GMBSZWXFWPHHUCXSDYLLCBBCBTKZUBVCBQRMCMXEFNEYGCFY'
     n_epochs = 2
     n_obs = 2
+    ndata = 200
 else:
     token = 'JGTKFUYDJMOKBMDFXICMGNEFBXOOSIPAVSGUWPSMJCVDWYMA'
     n_epochs = 50 
     n_obs = 1000
+    ndata = 10000
 
 create_dir(params['logdir'])
 
@@ -34,18 +36,19 @@ if params['id'] == None:
             dict(name='n_basis', type='int', bounds=dict(min=32, max=256)),
             dict(name='n_rbf', type='int', bounds=dict(min=5, max=16)),
             dict(name='activation', type='categorical', categorical_values=["ReLU", "shifted_softplus", "LeakyReLU", "swish", "ELU"]),
-            dict(name='cg_cutoff', type='int', bounds=dict(min=4.0, max=5.0)),
+            dict(name='cg_cutoff', type='double', bounds=dict(min=4.0, max=5.0)),
             dict(name='atom_cutoff', type='double', bounds=dict(min=3.0, max=4.5)),
             dict(name='enc_nconv', type='int', bounds=dict(min=2, max=5)),
-            dict(name='dec_nconv', type='int', bounds=dict(min=2, max=5)),
+            dict(name='dec_nconv', type='int', bounds=dict(min=2, max=7)),
             dict(name='beta', type='double', bounds=dict(min=0.0001, max=0.01), transformation="log"),
             dict(name='lr', type='double', bounds=dict(min=0.00001, max=0.001), transformation="log"),
-            dict(name='dir_mp', type='categorical', categorical_values=["True", "False"]),
+            dict(name='cg_mp', type='categorical', categorical_values=["True", "False"]),
+            dict(name='dec_type', type='categorical', categorical_values=["EquivariantDecoder", "ENDecoder"]),
         ],
         observation_budget=n_obs, # how many iterations to run for the optimization
         parallel_bandwidth=10,
     )
-    
+
 elif type(params['id']) == int:
     experiment = conn.experiments(params['id']).fetch()
 
@@ -55,30 +58,42 @@ while experiment.progress.observation_count < experiment.observation_budget:
     suggestion = conn.experiments(experiment.id).suggestions().create()
     trial =  suggestion.assignments
 
-    if trial['dir_mp'] == 'True':
-        dir_mp_flag = True 
+    # if trial['dir_mp'] == 'True':
+    #     dir_mp_flag = True 
+    # else:
+    #     dir_mp_flag = True
+
+    if trial['cg_mp'] == 'True':
+        cg_mp_flag = True 
     else:
-        dir_mp_flag = True
+        cg_mp_flag = False
 
     trial['logdir'] = os.path.join(params['logdir'], suggestion.id)
     trial['device'] = params['device']
     trial['dataset'] = 'dipeptide'
     trial['batch_size'] = 64
     trial['nepochs'] = n_epochs
-    trial['ndata'] = 10000
+    trial['ndata'] = ndata
     trial['nsamples'] = 200
     trial['n_cgs'] = 7
     trial['nsplits'] = 3
     trial['randommap'] = False
     trial['shuffle'] = False
     trial['optimizer'] = 'adam'
-    trial['dir_mp'] = dir_mp_flag
+    trial['dir_mp'] = False
+    trial['cg_mp'] =  cg_mp_flag
 
-    cv_score = run_cv(trial)
+    cv_mean, cv_std = run_cv(trial)
+    if np.isnan(cv_mean):
+        fail_flag = True
+    else:
+        fail_flag = False 
 
     conn.experiments(experiment.id).observations().create(
       suggestion=suggestion.id,
-      value=cv_score,
+      value=cv_mean,
+      value_stddev=cv_std,
+      failed=fail_flag
     )
 
     experiment = conn.experiments(experiment.id).fetch()
