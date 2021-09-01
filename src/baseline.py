b7bd25fb3d8f050e3094a5b9c1a390a99545a8ef
wwang2 <wwj@mit.edu>
2021-09-01 18:39:07 -0400

    trans-invariant regressiong

--- src/baseline.py
+++ src/baseline.py
@@ -27,3 +27,35 @@ class Baseline(nn.Module):
         x_recon = torch.einsum("bce,ca->bae", cg_xyz, self.B)
         
         return soft_assign, xyz, x_recon
+
+
+class EquiLinear(nn.Module):
+    def __init__(self, pooler, n_cgs, n_atoms):
+        nn.Module.__init__(self)
+        self.pooler = pooler 
+        self.B = nn.Parameter(torch.zeros(n_atoms, n_cgs ** 2))
+        
+    def forward(self, batch):
+    
+        xyz = batch['xyz']        
+        device = xyz.device
+        
+        z = batch['z'] # torch.ones_like( batch['z'] ) 
+        nbr_list = batch['nbr_list']
+
+        soft_assign, h, H, adj, cg_xyz, soft_cg_adj = self.pooler(z, 
+                                                                   batch['xyz'], 
+                                                                   batch['bonds'], 
+                                                                   tau=0.0,
+                                                                   gumbel=True)
+    
+    
+        basis = cg_xyz.unsqueeze(1) - cg_xyz.unsqueeze(2)
+        
+        basis = basis.reshape(h.shape[0], -1, 3)
+        
+        dx = xyz - cg_xyz[:, self.pooler.assign_idx, :]
+        
+        dx_recon = torch.einsum("ije,nj->ine", basis, self.B )
+        
+        return dx, dx_recon 
