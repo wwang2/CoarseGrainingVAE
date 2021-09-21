859b1edc3977ff23937eb3ade0a1f1d14e234114
wwang2 <wwj@mit.edu>
2021-09-21 13:14:49 -0400

    modify linear baseline

--- src/baseline.py
+++ src/baseline.py
@@ -8,7 +8,7 @@ class Baseline(nn.Module):
     def __init__(self, pooler, n_cgs, n_atoms):
         nn.Module.__init__(self)
         self.pooler = pooler 
-        self.B = nn.Parameter(0.0 * torch.randn(n_cgs, n_atoms))
+        self.B = nn.Parameter(0.01 * torch.randn(n_cgs, n_atoms))
         
     def forward(self, batch):
     
@@ -18,7 +18,7 @@ class Baseline(nn.Module):
         z = batch['z'] # torch.ones_like( batch['z'] ) 
         nbr_list = batch['nbr_list']
 
-        soft_assign, h, H, adj, cg_xyz, soft_cg_adj = self.pooler(z, 
+        soft_assign, assign_norm, h, H, adj, cg_xyz, soft_cg_adj = self.pooler(z, 
                                                                    batch['xyz'], 
                                                                    batch['bonds'], 
                                                                    tau=0.0,
