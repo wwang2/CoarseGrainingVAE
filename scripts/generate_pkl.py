d3df026407e9de501dd67d80c887624a87c0008d
wwang2 <wwj@mit.edu>
2021-11-06 05:07:49 -0400

    generate script with args

--- scripts/generate_pkl.py
+++ scripts/generate_pkl.py
@@ -7,18 +7,20 @@ from sampling import get_bond_graphs
 import sidechainnet as scn
 
 
-params = {'edgeorder': 2, "n_data": 1000000}
 
-thinning = 30
 
-for caspversion in [12]:
+def generate_data(params):
 
+    params['edgeorder'] = 2 
+    params['n_data'] = 1000000
+
+    thinning = params['thinning']
+    caspversion = params['caspversion']
 
     print("generating CASP{}".format(caspversion))
     data = scn.load(casp_version=caspversion, thinning=thinning)
 
     params['dataset'] = 'casp{}'.format(caspversion)
-
 
     train_path = os.path.join("../data/",  params['dataset'] + '_{}_{}_all.pkl'.format("train", thinning))
     val_path = os.path.join("../data/",  params['dataset'] + '_{}_{}_all.pkl'.format("val", thinning))
@@ -28,8 +30,16 @@ for caspversion in [12]:
     val_props = get_sidechainet_props(data['valid-10'], params, n_data=params['n_data'], split='valid-10', thinning=thinning)
     test_props = get_sidechainet_props(data['test'], params, n_data=params['n_data'], split='test', thinning=thinning)
 
-
     pickle.dump( train_props, open( train_path, "wb" ) )
     pickle.dump( val_props, open( val_path, "wb" ) )
     pickle.dump( test_props, open( test_path, "wb" ) )
         
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-caspversion", type=int, default=12)
+    parser.add_argument("-thinning", type=str, default=30)
+    params = vars(parser.parse_args())
+
+    generate_data(params)
