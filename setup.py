85682dec60b6065969a90aef3e20552a24dbb312
wwang2 <wwj@mit.edu>
2022-06-12 16:13:56 -0400

    add setup tool

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='CGVAE',
+   version='0.1',
+   description='',
+   author='Wujie Wang',
+   author_email='wwj@mit.edu',
+   packages=['src'],  #same as name
+   #install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
